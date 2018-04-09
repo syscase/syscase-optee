b04deb6b42f76f987f6c2eb44a0b17d67ccfbb3a
Firas Zaidan <firas@zaidan.de>
2018-04-09 14:45:42 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 root=$(dirname "$(readlink -f "$0")")
 
@@ -20,7 +20,7 @@ function make_toolchains {
 
 function make_smcchar {
   cd "$root/optee_examples/agent/smcchar"
-  build.sh
+  ./build.sh
 }
 
 function make_optee {
