c6ac19752b2982d345d32021606555787c0031c5
Firas Zaidan <firas@zaidan.de>
2018-07-29 18:35:57 +0200

    Refactor `qemu-system-aarch64-afl` to log to AFL's `outputs` folder
    * Add ENV `AFL_OUTPUT_PATH` to enable logging to AFL's `outputs` folder
    * Add ENV `QEMU_TERMINAL` to select terminal type:
        * `xterm` (default for `start`)
        * `screen` (default for `start-master` / `start-secondary`)
        * `xfce4-terminal`
    * Fix disabling of core dumps

---
+++ scripts/ncwait.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# ncwait PORT
+function ncwait {
+  while ! nc --telnet 127.0.0.1 $1; do sleep 1; done
+}
+
+# ncwait_log PORT LOGFILE
+function ncwait_log {
+  ncwait $1 | tee -a "$2"
+}
+
+# ncwait_log_gz PORT LOGFILE
+function ncwait_log_gz {
+  ncwait $1 | tee -a >(gzip --stdout >> "$2")
+}
