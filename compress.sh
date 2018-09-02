cc7ba389770448f9e983ef6730812979317327fe
Firas Zaidan <firas@zaidan.de>
2018-09-02 18:39:30 +0200

    Change `compress.sh` to use tar.xz

--- compress.sh
+++ compress.sh
@@ -5,6 +5,6 @@ BASE_PATH=$(dirname "$(readlink -f "$0")")
 cd "$BASE_PATH/task/outputs"
 
 while true; do
-  7z a -t7z -m0=lzma2 -mx=9 -md128m -mfb=256 -sdel "cov-$(date +"%F-%H%M%S").7z" "*/coverage/*.*"
+  tar --create --xz --remove-files --file "cov-$(date +"%F-%H%M%S").tar.xz" "*/coverage/*.*"
   sleep 10m
 done
